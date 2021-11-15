9af48b5f77b5aba7d04de6121dbfba31a3e7a977
rey <76417735+arrayrey@users.noreply.github.com>
2021-11-16 01:08:13 +0700

    Add files via upload

---
+++ Adapter Pattern/Kweek.java
@@ -0,0 +1,6 @@
+public class Kweek implements Bebek {
+    @Override
+    public void kweek() {
+        System.out.println("Kweekk Kwek!!!!");
+    }
+}
---
+++ Adapter Pattern/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main (String[] args) {
+        Kucing kucing = new Miaw();
+
+        Bebek miawAdapter = new MiawAdapter(kucing);
+        System.out.println("Kweekk Kwek!!!");
+        miawAdapter.kweek();
+    }
+}
